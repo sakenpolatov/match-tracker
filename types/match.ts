9b863210cd18bcb081ebb5e0ea82f2daba38bcc6
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-03-04 23:45:17 +0500

    types : match types

---
+++ types/match.ts
@@ -0,0 +1,24 @@
+export type MatchStatus = "Scheduled" | "Ongoing" | "Finished";
+
+export interface Player {
+  username: string;
+  kills: number;
+}
+
+export interface Team {
+  name: string;
+  players: Player[];
+  points: number;
+  place: number;
+  total_kills: number;
+}
+
+export interface Match {
+  time: string;
+  title: string;
+  homeTeam: Team;
+  awayTeam: Team;
+  homeScore: number;
+  awayScore: number;
+  status: MatchStatus;
+}
