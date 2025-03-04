32d1afba223c5259be62420dd524607b38bd8739
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-03-05 01:32:33 +0500

    style : add Inter font-family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { QueryProvider } from "@/services/QueryProvider";
+import { Inter } from "next/font/google";
 
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 export const metadata: Metadata = {
   title: "Match Tracker",
 };
@@ -12,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>
         <QueryProvider>{children}</QueryProvider>
       </body>
