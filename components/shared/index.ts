efdb1f06cda739c19f851c826fdd97d6de60f850
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-03-05 02:11:01 +0500

    refactor : re-exp

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,6 +1,9 @@
 export { Container } from "./container";
+export {ErrorMessage} from "./error-message"
 export { Header } from "./header";
 export { MatchCard } from "./matchCard";
 export { MatchList } from "./matchList";
+export {MatchStatus } from "./matchStatus";
+export {NoData} from "./no-data";
 export { RefreshButton } from "./refresh-button";
 export { Title } from "./title";
