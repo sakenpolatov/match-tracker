2d95c6884bcef6d45bbe3049e91be66a2fa3005e
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-03-04 23:30:04 +0500

    feat : homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/shared/header";
+import MatchList from "@/components/shared/matchList";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-2xl font-bold mb-4">Match Tracker</h1>
-
-      <div className="flex gap-4 items-center">
-        <img src="/icons/alert.svg" alt="Alert Icon" className="w-6 h-6 text-red-500" />
-        <Button 
-          type="button" 
-          className="flex items-center gap-2 bg-red-500 hover:bg-red-600 cursor-pointer text-white px-4 py-2 rounded focus:outline-none"
-        >
-          <img src="/icons/Refresh.svg" alt="Refresh Icon" className="w-4 h-4" />
-          Обновить
-        </Button>
-      </div>
-    </div>
+    <main className="min-h-screen bg-[#06080C] w-full">
+      <Header />
+      <MatchList />
+    </main>
   );
 }
