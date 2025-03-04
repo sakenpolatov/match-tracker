d9dced29e25d49fec35b38b429fb45f56a7eeac8
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-03-05 00:47:55 +0500

    feat : add skeleton

--- components/shared/matchList.tsx
+++ components/shared/matchList.tsx
@@ -3,21 +3,34 @@
 import { useMatches } from "@/hooks/useMatches";
 import { Container } from "./container";
 import { MatchCard } from "./matchCard";
+import { Skeleton } from "@/components/ui/skeleton"; 
 
 export const MatchList = () => {
   const { data: matches = [], isLoading, error } = useMatches();
+	const skeletonCount = matches.length || 7;
 
-  if (isLoading) return <p className="text-white">Загрузка...</p>;
-  if (error) return <p className="text-red-500">Ошибка загрузки данных</p>;
+  if (isLoading) {
+    return (
+      <Container>
+        <div className="w-full mt-[32px] flex flex-col gap-[12px]">
+          {[...Array(skeletonCount)].map((_, index) => (
+            <Skeleton key={index} className="h-[87px] w-full bg-[#0B0E12] rounded-md animate-pulse" />
+          ))}
+        </div>
+      </Container>
+    );
+  }
+
+  if (error) {
+    return <p className="text-red-500">Ошибка загрузки данных</p>;
+  }
 
   return (
     <Container>
-      <div className="w-full mt-[32px]">
-        <div className="flex flex-col gap-[12px]">
-          {matches.map((match) => (
-            <MatchCard key={match.title} match={match} />
-          ))}
-        </div>
+      <div className="w-full mt-[32px] flex flex-col gap-[12px]">
+        {matches.map((match) => (
+          <MatchCard key={match.title} match={match} />
+        ))}
       </div>
     </Container>
   );
