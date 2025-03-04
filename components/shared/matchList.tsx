e57c81c6baaf94d5fd93e097dd8743d6d0288138
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-03-04 23:31:17 +0500

    feat : add matchList

---
+++ components/shared/matchList.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useMatches } from "@/hooks/useMatches";
+import { Container } from "./container";
+import MatchCard from "./matchCard";
+
+export default function MatchList() {
+  const { data: matches = [], isLoading, error } = useMatches();
+
+  if (isLoading) return <p className="text-white">Загрузка...</p>;
+  if (error) return <p className="text-red-500">Ошибка загрузки данных</p>;
+
+  return (
+    <Container>
+      <div className="w-full mt-[32px]">
+        <div className="flex flex-col gap-[12px]">
+          {matches.map((match) => (
+            <MatchCard key={match.title} match={match} />
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
+}
