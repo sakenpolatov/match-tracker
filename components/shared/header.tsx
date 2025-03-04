2b3fa10400bf9eb922217f5cbd3bb01f47c77cb1
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-03-05 00:47:38 +0500

    refactor :  re-exp

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,13 +2,17 @@
 
 import { Container } from "./container";
 import { RefreshButton } from "./refresh-button";
+import { Title } from "./title";
+
 
 export const Header = () => (
   <header className="bg-[#06080C] py-6 shadow-md w-full">
     <Container className="flex justify-between items-center">
-      <h1 className="text-white text-[32px] leading-[32px] font-[TacticSans] italic">
-        Match Tracker
-      </h1>
+      <Title
+        text="Match Tracker"
+        size="lg"
+        className="text-white font-[TacticSans] italic leading-[32px]"
+      />
       <RefreshButton />
     </Container>
   </header>
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -3,3 +3,4 @@ export { Header } from "./header";
 export { MatchCard } from "./matchCard";
 export { MatchList } from "./matchList";
 export { RefreshButton } from "./refresh-button";
+export { Title } from "./title";
