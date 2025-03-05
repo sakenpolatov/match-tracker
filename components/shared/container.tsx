4ef8d0ee0d8409ce8f674b02a2e1a87a2f9940b5
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2025-03-05 05:37:04 +0500

    fix : change width container

--- components/shared/container.tsx
+++ components/shared/container.tsx
@@ -10,7 +10,7 @@ export const Container: React.FC<React.PropsWithChildren<Props>> = ({
   children,
 }) => {
   return (
-    <div className={cn("mx-auto max-w-[1280px] px-4 w-full", className)}>
+    <div className={cn("mx-auto max-w-[1836px] px-4 w-full", className)}>
       {children}
     </div>
   );
